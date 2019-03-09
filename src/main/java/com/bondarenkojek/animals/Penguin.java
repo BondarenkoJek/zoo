dfdd0796f02bdbe415287f93046fe1a7aa1d60b2
bondarenkojek <bondarenkojek@gmail.com>
2019-03-09 13:59:14 +0200

    fix bag

--- src/main/java/com/bondarenkojek/animals/Penguin.java
+++ src/main/java/com/bondarenkojek/animals/Penguin.java
@@ -1,11 +1,18 @@
 package com.bondarenkojek.animals;
 
-public class Penguin implements Bird {
+public class Penguin implements Bird, Fish {
+    @Override
     public void eat() {
         System.out.println("Penguin is eating!!!");
     }
 
+    @Override
     public void fly() {
-        System.out.println("Penguin can not fly! Penguin is swimming!!! ");
+        System.out.println("Penguin can not fly!");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Penguin is swimming!!!");
     }
 }
